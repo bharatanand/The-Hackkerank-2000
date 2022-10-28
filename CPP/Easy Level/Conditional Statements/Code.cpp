35bb360448121c697e997a8fe65ccfb531bbfcb5
bharatanand <bharatanand@outlook.in>
2022-10-28 13:58:41 +0530

    well about that

--- CPP/Easy Level/Conditional Statements/Code.cpp
+++ CPP/Easy Level/Conditional Statements/Code.cpp
@@ -1,3 +1,40 @@
+
+#include <bits/stdc++.h> 
+//It is basically a header file that includes every standard library. 
+//In programming contests, using this file is a good idea, when you want to reduce the time wasted in doing chores; 
+//especially when your rank is time sensitive. 
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    // Write Your Code Here
+    if (n==1)
+    cout<<"one";
+    else if(n==2)
+    cout<<"two";
+    else if(n==3)
+    cout<<"three";
+    else if(n==4)
+    cout<<"four";
+    else if(n==5)
+    cout<<"five";
+    else if(n==6)
+    cout<<"six";
+    else if(n==7)
+    cout<<"seven";
+    else if(n==8)
+    cout<<"eight";
+    else if(n==9)
+    cout<<"nine";
+    else
+    cout<<"Greater than 9";
+    return 0;
+}
+
 /*
 #include <iostream>
 using namespace std;
@@ -65,39 +102,3 @@ int main()
 }
 
 */
-
-#include <bits/stdc++.h> 
-//It is basically a header file that includes every standard library. 
-//In programming contests, using this file is a good idea, when you want to reduce the time wasted in doing chores; 
-//especially when your rank is time sensitive. 
-using namespace std;
-
-int main()
-{
-    int n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    // Write Your Code Here
-    if (n==1)
-    cout<<"one";
-    else if(n==2)
-    cout<<"two";
-    else if(n==3)
-    cout<<"three";
-    else if(n==4)
-    cout<<"four";
-    else if(n==5)
-    cout<<"five";
-    else if(n==6)
-    cout<<"six";
-    else if(n==7)
-    cout<<"seven";
-    else if(n==8)
-    cout<<"eight";
-    else if(n==9)
-    cout<<"nine";
-    else
-    cout<<"Greater than 9";
-    return 0;
-}
